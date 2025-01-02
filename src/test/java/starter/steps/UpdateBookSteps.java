e16fdad48ce779acc527a82d28e518953a421e95
taneeshakal <taneeshakal@gmail.com>
2025-01-02 23:03:44 +0530

    PUT API testing, update with missing title and missing author

--- src/test/java/starter/steps/UpdateBookSteps.java
+++ src/test/java/starter/steps/UpdateBookSteps.java
@@ -17,40 +17,35 @@ public class UpdateBookSteps {
     private String username;
     private String password;
 
-    // login
-    @Given("I am logged in with the username {string} and password {string}")
+    // Login
+    @Given("I am logged in with the username {string} and password {string} - Update Book")
     public void i_am_logged_in_with_the_username_and_password(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
-    // create book if it doesn't exist
-    @Given("I have a book with ID {string}, title {string}, and author {string}")
-    public void i_have_a_book_with_id_title_and_author(String bookId, String title, String author) {
-        // Check if the book exists, if not create it
-        Response getResponse = given()
-                .auth().basic(username, password)
-                .when()
-                .get(baseUrl + "/api/books/" + bookId);
+    // Create a book
+    @Given("I have created a book with ID {int}, title {string}, and author {string} - Update Book")
+    public void i_have_created_a_book_with_id_title_and_author(Integer bookId, String title, String author) {
+        String jsonBody = String.format("{\"id\": %d, \"title\": \"%s\", \"author\": \"%s\"}", bookId, title, author);
 
-        if (getResponse.statusCode() == 404) {
-            String jsonBody = String.format("{\"id\": \"%s\", \"title\": \"%s\", \"author\": \"%s\"}", bookId, title, author);
-            given()
-                    .auth().basic(username, password)
-                    .contentType("application/json")
-                    .body(jsonBody)
-                    .when()
-                    .post(baseUrl + "/api/books");
-        }
+        given()
+                .auth().basic(username, password)
+                .contentType("application/json")
+                .body(jsonBody)
+                .when()
+                .post(baseUrl + "/api/books")
+                .then()
+                .statusCode(201); // Ensure the book is created successfully
     }
 
-    // update book details
-    @When("I update the book with ID {string} to title {string} and author {string}")
-    public void i_update_the_book_with_id_to_title_and_author(String bookId, String title, String author) {
+    // Update book details
+    @When("I update the book with ID {int} to title {string} and author {string} - Update Book")
+    public void i_update_the_book_with_id_to_title_and_author(Integer bookId, String title, String author) {
         expectedTitle = title;
         expectedAuthor = author;
 
-        String jsonBody = String.format("{\"id\": \"%s\", \"title\": \"%s\", \"author\": \"%s\"}", bookId, title, author);
+        String jsonBody = String.format("{\"id\": %d, \"title\": \"%s\", \"author\": \"%s\"}", bookId, title, author);
 
         response = given()
                 .auth().basic(username, password)
@@ -60,14 +55,14 @@ public class UpdateBookSteps {
                 .put(baseUrl + "/api/books/" + bookId);
     }
 
-    // validate response status code
-    @Then("the response status code should be {int}")
+    // Validate response status code
+    @Then("the response status code should be {int} - Update Book")
     public void the_response_status_code_should_be(Integer expectedStatusCode) {
         assertThat(response.statusCode(), equalTo(expectedStatusCode));
     }
 
-    // validate updated book details
-    @Then("the book should be updated with the new details")
+    // Validate updated book details
+    @Then("the book should be updated with the new details - Update Book")
     public void the_book_should_be_updated_with_the_new_details() {
         String updatedTitle = response.jsonPath().getString("title");
         String updatedAuthor = response.jsonPath().getString("author");
@@ -75,4 +70,12 @@ public class UpdateBookSteps {
         assertThat(updatedTitle, equalTo(expectedTitle));
         assertThat(updatedAuthor, equalTo(expectedAuthor));
     }
+
+    // Validate error message for invalid input
+    @Then("Invalid | Empty Input Parameters in the Request should be indicated - Update Book")
+    public void invalid_empty_input_parameters_in_the_request_should_be_indicated() {
+        String errorMessage = response.jsonPath().getString("error");
+        assertThat(errorMessage, containsString("Invalid | Empty Input Parameters"));
+    }
+
 }
