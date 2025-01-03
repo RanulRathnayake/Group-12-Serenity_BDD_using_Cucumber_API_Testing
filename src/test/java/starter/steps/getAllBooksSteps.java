9f9190f2f07cc81e48b446feafa7017c16fff15b
Thathsarani <hasarekat@gmail.com>
2025-01-03 15:52:52 +0530

    GET API testing updated the steps

--- src/test/java/starter/steps/getAllBooksSteps.java
+++ src/test/java/starter/steps/getAllBooksSteps.java
@@ -31,7 +31,7 @@ public class getAllBooksSteps {
     }
 
     // When step - request to get all books
-    @When("I request to get all books")
+    @When("I request to get all books - GET")
     public void i_request_to_get_all_books() {
         if (username != null && password != null) {
             response = given()
@@ -45,12 +45,6 @@ public class getAllBooksSteps {
         }
     }
 
-    // Then step - validate response status code for successful get
-    @Then("the response status code should be {int}")
-    public void the_response_status_code_should_be(Integer expectedStatusCode) {
-        assertThat(response.statusCode(), equalTo(expectedStatusCode));
-    }
-
     // Then step - validate the response contains a list of books
     @And("the response should contain a list of books")
     public void the_response_should_contain_a_list_of_books() {
@@ -60,18 +54,47 @@ public class getAllBooksSteps {
     // Then step - validate error message for missing authentication
     @And("the response should indicate that authentication is required")
     public void the_response_should_indicate_that_authentication_is_required() {
-        // Ensure the response has a 401 status code first
         assertThat(response.statusCode(), equalTo(401));
-
-        // Check if the response body is empty
         String responseBody = response.body().asString();
         if (responseBody.isEmpty()) {
-            // If the response body is empty, handle accordingly (you can add more validation here)
             System.out.println("Response body is empty. Authentication is required.");
         } else {
-            // If the body is not empty, parse the error message
             String errorMessage = response.jsonPath().getString("message");
             assertThat(errorMessage, containsString("Authentication is required"));
         }
     }
+
+    // Given step - successfully fetch books with valid credentials
+    @And("I request to get all books successfully")
+    public void i_request_to_get_all_books_successfully() {
+        response = given()
+                .auth().basic(username, password)
+                .when()
+                .get(baseUrl + "/api/books");
+        assertThat(response.statusCode(), equalTo(200));
+        assertThat(response.jsonPath().getList(""), is(not(empty())));
+    }
+
+    // When step - remove the password and request again
+    @And("I remove the password and request to get all books again")
+    public void i_remove_the_password_and_request_to_get_all_books_again() {
+        this.password = null; // Remove the password
+        if (username != null && password == null) {
+            response = given()
+                    .when()
+                    .get(baseUrl + "/api/books");
+        } else if (username != null && password != null) {
+            response = given()
+                    .auth().basic(username, password)
+                    .when()
+                    .get(baseUrl + "/api/books");
+        }
+    }
+
+    // Then step - validate response is unauthorized for missing password
+    @Then("the response status code should be {int}")
+    public void the_response_status_code_should_be(Integer expectedStatusCode) {
+        assertThat(response.statusCode(), equalTo(expectedStatusCode));
+    }
+
 }
